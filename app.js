1d2169c8fdb0c0152704618c257fe8a57372113d
Nitin Singhal <nitin.singhal@gmail.com>
2016-06-23 11:30:15 -0700

    6/22 work

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-require('./archive/instantHello');
-var goodbye = require("./talk/goodbye");
-var talk = require("./talk");
-var question = require("./talk/question");
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
 
-talk.intro();
-talk.hello("Nitin");
+var routes = require("./api/routes");
 
-console.log(question.ask("What is the meaning of life?"));
-goodbye();
+app.set('port', 8080);
 
-//console.log("It works!");
+//'use' as middleware
+app.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
+
+// Atuomatically send static files using express
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use bodyParser here to parse form body for /apis. Putting it here will ignore static content calls.
+app.use(bodyParser.urlencoded({
+	extended: false // we only need strings and arrays from the form body
+}));
+
+// Using express routes
+app.use("/api", routes);
+
+var server = app.listen(app.get('port'), function() {
+	console.log(server.address());
+	var port = server.address().port;
+	console.log("Magic happens on port " + port);
+});
